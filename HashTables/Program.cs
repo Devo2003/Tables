1a33eb30db69f6b780c9206be59f0f92705105dc
Devo2003 <93745734+Devo2003@users.noreply.github.com>
2023-10-25 22:49:19 -0500

    adding to repo

---
+++ HashTables/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using static System.Console;
+
+namespace HashTables 
+{
+    public class Program
+    {
+        static void Main()
+        {
+            hashVSarray hs = new hashVSarray();
+
+            hs.StackNqueue();
+            //hs.ArrayNmaps();
+            //hs.getFile();
+            Console.ReadKey();
+        }
+    }
+}
+
---
+++ HashTables/hashVSarray.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections;
+
+namespace HashTables
+{
+	public class hashVSarray
+	{
+
+
+        public string lines;
+        public string fileDirectory;
+        public string[] fileLines;
+        public Hashtable Numbers = new Hashtable();
+        public Stack<string> myStack = new Stack<string>();
+        public Queue qt = new Queue();
+
+        public hashVSarray()
+		{
+            //this line gives in the path of the file into a string
+            fileDirectory = "/Users/devinsalgado35/Desktop/HashTables/HashTables/Nums.txt";
+        }
+        public void ArrayNmaps()
+        {
+            fileLines = File.ReadAllLines(fileDirectory);
+            //adds the file items into the array
+            foreach (string line in fileLines)
+            {
+                Console.WriteLine(line);
+            }
+            //adds the file items into the Hashtable
+            foreach (string Numbers in fileLines)
+            {
+                
+                Console.WriteLine(Numbers);
+            }
+        }
+        public void StackNqueue()
+        {
+            fileLines = File.ReadAllLines(fileDirectory);
+            //adds the file items into the stack
+            foreach (string item in fileLines)
+            {
+                myStack.Push(item);
+                Console.WriteLine(item);
+            }
+            //adds the file items into the queue
+            foreach (string item in fileLines)
+            {
+                qt.Enqueue(item);
+                Console.WriteLine(qt);
+            }
+        }
+        //this checks if the file is not found, or unable to read a file in that directory
+        public void fileMissing()
+        {
+            if (!File.Exists(fileDirectory))
+            {
+                Console.WriteLine("File not found");
+            }
+        }
+    }
+}
+
